db312786f0eaa7ed2d9ec77bb45e7e693ef7a652
nekokaito <siddiqsazzad001@gmail.com>
2024-01-25 19:50:31 +0600

    op js

---
+++ Basics/number.js
@@ -0,0 +1,6 @@
+var redmiPh = 550;
+var redmiph2 = parseInt("54");
+
+console.log(redmiPh+redmiph2);
+
+console.log((0.1+0.2).toFixed(2));
---
+++ Basics/opration.js
@@ -0,0 +1,13 @@
+var a = 10;
+var b =20;
+
+var sum = a+b;
+var sub = a-b;
+var mul = a*b;
+var div = a/b;
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(a%b); //Mod
