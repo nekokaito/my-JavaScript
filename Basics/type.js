3e2fa6bd65a446ec171f94259f106760e9f877bd
nekokaito <siddiqsazzad001@gmail.com>
2024-01-25 19:20:35 +0600

    variable and type js

---
+++ Basics/type.js
@@ -0,0 +1,9 @@
+// String
+var name = "kaito";
+var address = "161 Dhaka";
+
+//Boolean
+var isLove = true;
+var isEvil = false;
+console.log(isLove);
+console.log(typeof isLove)
---
+++ Basics/variable.js
@@ -0,0 +1,5 @@
+var year = 2000;
+var Name = "Kaito";
+
+console.log(year);
+console.log(Name);
