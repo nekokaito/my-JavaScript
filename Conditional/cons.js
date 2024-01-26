ebcb6819933d2d7b2fa2e22a6adf852283872475
nekokaito <siddiqsazzad001@gmail.com>
2024-01-26 22:26:14 +0600

    if else

--- Conditional/cons.js
+++ Conditional/cons.js
@@ -2,3 +2,7 @@ console.log(9>10);
 console.log(10>5);
 console.log(5==5);
 console.log (12>=12);
+
+console.log(5=='5');
+console.log(5 ==='5');
+console.log(5 === 5);
---
+++ Conditional/if-else.js
@@ -0,0 +1,8 @@
+var a = 40;
+
+if (a%2 == 0) {
+    console.log("Even")
+}
+else {
+    console.log("Odd")
+}
