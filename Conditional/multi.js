ebe660df29fb4bba9ef1b591a98d8ca97c9d182a
nekokaito <siddiqsazzad001@gmail.com>
2024-01-26 22:38:34 +0600

    multi js

---
+++ Conditional/multi.js
@@ -0,0 +1,12 @@
+const userMoney = 15000;
+const hasPC = true;
+
+ if (userMoney>10000 && hasPC == true) {
+    console.log("You are welcome to buy Monitor");
+ }
+ else if (hasPC == true && userMoney< 10000) {
+    console.log("Earn Money First");
+ }
+ else {
+    console.log("Buy Pc First Noob and earn money");
+ }
