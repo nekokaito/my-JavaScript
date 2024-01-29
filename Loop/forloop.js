94bf0ec1b05fe505689858d1d32b8097173284dd
nekokaito <siddiqsazzad001@gmail.com>
2024-01-29 21:18:33 +0600

    while js

--- Loop/forloop.js
+++ Loop/forloop.js
@@ -3,3 +3,6 @@ const a = [10,12,13,14];
 for (const i of a) {
     console.log(i);
 }
+for (let i=0; i<10 ; i++) {
+    console.log("I love me"); 
+}
---
+++ Loop/while.js
@@ -0,0 +1,6 @@
+let x = 0;
+
+while (x<10) {
+    console.log("Code is Fun");
+    x++;
+}
