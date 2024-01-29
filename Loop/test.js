d62f717aa08cd3fe8ccfeb3b93df091e2fc97f7d
nekokaito <siddiqsazzad001@gmail.com>
2024-01-29 22:10:46 +0600

    js pack

---
+++ Loop/test.js
@@ -0,0 +1,16 @@
+// let num = 10;
+
+// while (num--) {
+//  console.log(num);
+// }
+// for (let i = 0; i < 5; i+=3) {
+//     console.log(i);
+//   }
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+for (let i = 1; i < array.length; i++) {
+ if (i == 5) {
+  continue;
+ }
+ console.log(array[i]);
+}
