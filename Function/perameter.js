c54d9481764b9e917cef8af5ba17a12bdb0f18c8
nekokaito <siddiqsazzad001@gmail.com>
2024-02-02 20:42:07 +0600

    perameter js

---
+++ Function/perameter.js
@@ -0,0 +1,11 @@
+function square (value) {
+    let sq = value*value;
+    return sq;
+}
+console.log('Square is :', square(4));
+
+function sum (a,b) {
+    let sum = a+b;
+    console.log(sum);
+}
+sum(10,20);
