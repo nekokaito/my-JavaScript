2a411699939d7178a03adca53c677b6ba89e4b0e
nekokaito <siddiqsazzad001@gmail.com>
2024-02-03 20:58:57 +0600

    added more logic to leapyear js

--- Problems/leapYear.js
+++ Problems/leapYear.js
@@ -1,11 +1,17 @@
 function isLeapYear (year) {
-    
-    if ((year%4===0||year%400===0)&&(year%100 !== 0)) {
-        return true;
+
+    if (year === 1600 || year ===  2000 || year === 2400) {
+           return true;
     }
     else {
-        return false;
+        if ((year%4===0||year%400===0)&&(year%100 !== 0)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
+    
       
 }
-console.log(isLeapYear(2024));
+console.log(isLeapYear(1600));
