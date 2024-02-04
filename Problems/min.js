6aead3d8743a7baedffc618720526541a50db406
nekokaito <siddiqsazzad001@gmail.com>
2024-02-04 20:22:03 +0600

    problem min js

---
+++ Problems/min.js
@@ -0,0 +1,6 @@
+
+const ArifHas = 1205;
+const SiddiqHas = 508;
+const min = Math.min(ArifHas,SiddiqHas);
+
+console.log("Poor Boy is Siddiq who has ", min, " taka.");
---
+++ Problems/test.js
@@ -0,0 +1,15 @@
+// for (var i = 0; i < 5; i++) {
+//     setTimeout(function () {
+//      console.log(i);
+//   }, 100);
+// }  
+
+// console.log(Math.round(0.95));
+// console.log(Math.round(4));
+// console.log(Math.round(7.004));
+// console.log(Math.ceil(-7.004));
+
+function a (x,y,z) {
+    console.log(x,y,z); 
+}
+a(10,20);
