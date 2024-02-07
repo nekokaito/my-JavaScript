7a6ed90068ef6e2a97e79c2627009c726657656e
nekokaito <siddiqsazzad001@gmail.com>
2024-02-07 19:12:41 +0600

    problem 2 js done

--- Assignment/problem2.js
+++ Assignment/problem2.js
@@ -1,10 +1,18 @@
 function checkName(name) {
-   
-    if (typeof(checkName) === 'string') {
-          
-        
+    const size = name.length;
+    const lastLetter = name[size-1];
+    let type = '';
+    if (typeof(name) === 'string') {
+        if (lastLetter == 'a' || lastLetter == 'y' ||lastLetter == 'i' ||lastLetter == 'e' ||lastLetter == 'o' ||lastLetter == 'u' ||lastLetter == 'w' || lastLetter == 'A' || lastLetter == 'Y' ||lastLetter == 'I' ||lastLetter == 'E' ||lastLetter == 'O' ||lastLetter == 'U' ||lastLetter == 'W') {
+                type = 'Good Name';
+            }
+            else {
+                type = 'Bad Name';
+            }
+            return type;
+        } 
+        else {
+            return 'invalid';
+        } 
+
     }
-    else {
-        return 'invalid';
-    }
-}
